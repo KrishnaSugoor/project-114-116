0a60ff1036b3d5fdba118dbe2467e5b26870c88c
KrishnaSugoor <86477778+KrishnaSugoor@users.noreply.github.com>
2023-01-27 18:38:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,27 @@ function preload(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+   video.hide();  
+posenet=ml5.poseNet(video,loaded);
+posenet.on("pose",poselol);
 }
 function draw(){
-
+image(video,0,0,300,300);
 }
 function takesnap(){
-    save("you are spiderman.png");
-    
+    save("you are a spider.png");
+
+}
+function loaded(){
+    console.log("posenet initialized");
+}
+function poselol(result){
+if(result.length>0)
+{
+    console.log(result);
+    console.log("nose x= "+result[0].pose.nose.x);
+    console.log("nose y= "+result[0].pose.nose.y);
+}
 }
