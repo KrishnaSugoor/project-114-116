9e48cdfa6e670f4f4ab95814c4086edbcd17bccd
KrishnaSugoor <86477778+KrishnaSugoor@users.noreply.github.com>
2023-02-15 18:57:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+nosex=0;
+nosey=0;
 function preload(){
-
+    clownnose=loadImage("https://i.postimg.cc/C19LF2Lf/image-removebg-preview.png");
 }
 function setup(){
     canvas=createCanvas(300,300);
@@ -12,6 +14,7 @@ posenet.on("pose",poselol);
 }
 function draw(){
 image(video,0,0,300,300);
+image(clownnose, nosex, nosey, 200,200);
 }
 function takesnap(){
     save("you are a spider.png");
@@ -26,5 +29,7 @@ if(result.length>0)
     console.log(result);
     console.log("nose x= "+result[0].pose.nose.x);
     console.log("nose y= "+result[0].pose.nose.y);
+    nosex=result[0].pose.nose.x-87;
+   nosey=result[0].pose.nose.y-130;
 }
 }
